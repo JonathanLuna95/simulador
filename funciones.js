0dde5c549db688923beb856c6679c4334bb2bb1e
JonathanLuna95 <jjlunap95@gmail.com>
2026-04-15 23:27:47 -0500

    Valor disponible y capacidad pago en pantalla

--- funciones.js
+++ funciones.js
@@ -1,1 +1,20 @@
 //AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
+
+function calcularDisponible(ingresos, egresos){
+    let disponible = ingresos - egresos;
+    if(disponible < 0){
+        disponible = 0;
+    }
+    return disponible;
+}
+
+function calcularCapacidadPago(montoDisponible){
+    let capacidadDePago = montoDisponible/2;
+
+    return capacidadDePago;
+}
+
+function recuperarTexto(spn,valor){
+    let texto = document.getElementById(spn);
+    texto.innerText=valor.toFixed(2);
+}
--- simulador.js
+++ simulador.js
@@ -1,1 +1,17 @@
 //AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
+
+function calcular() {
+    let ingresos = parseFloat(document.getElementById("txtIngresos").value);
+    let egresos = parseFloat(document.getElementById("txtEgresos").value);
+ 
+    let disponible = calcularDisponible(ingresos, egresos);
+
+    //const elemento = document.getElementById("spnDisponible");
+    //elemento.innerText = disponible;
+
+    recuperarTexto("spnDisponible",disponible);
+ 
+    let capacidadDePago = calcularCapacidadPago(disponible);
+
+    recuperarTexto("spnCapacidadPago",capacidadDePago);
+}
